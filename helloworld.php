c0a922620758c8ade031b5c016f33194b46e55cf
whosaidnicole <whosaidnicole@greeny.cs.tlu.ee>
2016-02-11 12:44:33 +0200

    added first lines of PHP

--- helloworld.php
+++ helloworld.php
@@ -1,1 +1,14 @@
-hello
+<?php
+
+//variable starts with $,
+//string has to use quotation marks
+//line ends with semicolon
+$first_name = "Nicole";
+$last_name = "Susi";
+
+//show code to browser
+//for assing up numbers + sign
+//for adding up strings . 
+echo $first_name." ".$last_name;
+ 
+?>
